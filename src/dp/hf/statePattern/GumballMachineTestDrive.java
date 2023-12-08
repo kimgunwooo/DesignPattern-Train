e79d6f314427e5ad43dd9674c4b4f5b27d561097
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-12-08 18:15:42 +0900

    State Pattern

---
+++ src/dp/hf/statePattern/GumballMachineTestDrive.java
@@ -0,0 +1,51 @@
+package dp.hf.statePattern;
+
+import dp.hf.MyHWInfo;
+import dp.hf.statePattern.context.GumballMachine;
+
+public class GumballMachineTestDrive {
+
+	public static void main(String[] args) {
+		MyHWInfo.printInfo();
+
+		GumballMachine gumballMachine =
+			new GumballMachine(10);
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+	}
+}
